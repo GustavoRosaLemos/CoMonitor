e557c1ae21e446d051ddceb3535dbb3055697103
Gustavo <dev.gustavolemos@outlook.com>
2021-08-24 07:52:18 -0300

    #FA-003 Added PhoneModel and EmailModel.

--- src/model/EmailModel.java
+++ src/model/EmailModel.java
@@ -1,4 +1,25 @@
 package model;
 
 public class EmailModel {
+    private int id;
+    private String type;
+    private String value;
+
+    public EmailModel(int id, String type, String value) {
+        this.id = id;
+        this.type = type;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
