8e74e52f73b42f24a8ad46584e288644690faf91
Gustavo <dev.gustavolemos@outlook.com>
2021-08-26 02:17:18 -0300

    Removed CustomException.java.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import CustomException.CustomException;
-
 public class Main {
-    public static void main(String[] args) throws CustomException {
+    public static void main(String[] args) {
         System.out.println("Sistema iniciado!");
     }
 }
