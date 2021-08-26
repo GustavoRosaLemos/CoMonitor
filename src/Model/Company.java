b27a8366321723b44c30d4d8d7a20900704bf963
Gustavo <dev.gustavolemos@outlook.com>
2021-08-26 20:09:32 -0300

    Finished works.

--- src/Model/Company.java
+++ src/Model/Company.java
@@ -16,26 +16,8 @@ public class Company {
         return companyName;
     }
 
-    public boolean setCompanyName(String companyName) {
-        if(companyName instanceof String) {
-            this.companyName = companyName;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     public String getFantasyName() {
         return fantasyName;
-    }
-
-    public boolean setFantasyName(String fantasyName) {
-        if(fantasyName instanceof String) {
-            this.fantasyName = fantasyName;
-            return true;
-        } else {
-            return false;
-        }
     }
 
     public String getCnpj() {
